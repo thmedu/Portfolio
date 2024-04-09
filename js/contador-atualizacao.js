2914fd20e7e1bdbacc64afc629d8ba53b3476eee
Thomas Nascimento <thmedu@outlook.com>
2024-04-09 16:56:45 -0300

    Acresentando o  Email-Js no meu formulario

--- js/contador-atualizacao.js
+++ js/contador-atualizacao.js
@@ -1,12 +1,25 @@
+// Função para obter a data e hora atual formatada
 function getDataAtualFormatada() {
     var dataAtual = new Date();
     var dia = dataAtual.getDate();
-    var mes = dataAtual.toLocaleString('pt-BR', { month: 'long' }); // Obtém o nome do mês
+    var mes = dataAtual.getMonth() + 1; // Mês começa de zero, então adicionamos 1
     var ano = dataAtual.getFullYear();
     var hora = dataAtual.getHours();
     var minutos = dataAtual.getMinutes();
-
-    // Formato: DD de MMMM de AAAA 'às' HH:MM
-    var dataFormatada = dia + ' de ' + mes + ' de ' + ano + ' às ' + hora + ':' + (minutos < 10 ? '0' : '') + minutos;
+    
+    // Formato: DD.MM.AAAA HH:MM:SS
+    var dataFormatada = dia + '/' + mes + '/' + ano + ' ' + hora + ':' + minutos;
     return dataFormatada;
 }
+
+// Atualiza o texto para exibir a última vez que a página foi atualizada
+function atualizarUltimaAtualizacao() {
+    var ultimaAtualizacaoElemento = document.getElementById('ultima-atualizacao');
+    ultimaAtualizacaoElemento.textContent = getDataAtualFormatada();
+}
+
+// Chamada inicial para atualizar a última vez que a página foi atualizada
+atualizarUltimaAtualizacao();
+
+// Atualiza a cada 1 segundo
+setInterval(atualizarUltimaAtualizacao, 1000);
