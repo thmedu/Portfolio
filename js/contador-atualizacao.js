020eacc2db007d8cf0f879ad9955fcdb0612b00a
Thomas Nascimento <thmedu@outlook.com>
2024-04-30 22:18:58 -0300

    up

--- js/contador-atualizacao.js
+++ js/contador-atualizacao.js
@@ -1,42 +1,9 @@
-// Importa o módulo express, que é um framework web para Node.js
-const express = require('express');
-// Inicializa o aplicativo Express
-const app = express();
-
-// Rota para lidar com as solicitações do Webhook
-app.post('/webhook', (req, res) => {
-    // Atualiza a última vez que a página foi atualizada chamando a função definida mais tarde
-    atualizarUltimaAtualizacao();
-    // Envia uma resposta HTTP com o código de status 200 (OK)
-    res.sendStatus(200);
-});
-
-// Inicia o servidor na porta 3000 e imprime uma mensagem no console quando estiver pronto
-app.listen(3000, () => {
-    console.log('Servidor ouvindo na porta 3000');
-});
-
-// Função para obter a data e hora atual formatada
-function getDataAtualFormatada() {
-    // Obtém a data e hora atuais
-    var dataAtual = new Date();
-    // Extrai o dia, mês, ano, hora e minutos da data atual
-    var dia = dataAtual.getDate();
-    var mes = dataAtual.getMonth() + 1; // Mês começa de zero, então adicionamos 1
-    var ano = dataAtual.getFullYear();
-    var hora = dataAtual.getHours();
-    var minutos = dataAtual.getMinutes();
-    
-    // Formata a data e hora no formato: DD/MM/AAAA HH:MM
-    var dataFormatada = dia + '/' + mes + '/' + ano + ' ' + hora + ':' + minutos;
-    // Retorna a data formatada
-    return dataFormatada;
-}
-
-// Função para atualizar o texto que exibe a última vez que a página foi atualizada
-function atualizarUltimaAtualizacao() {
-    // Obtém uma referência ao elemento HTML com o ID 'ultima-atualizacao'
-    var ultimaAtualizacaoElemento = document.getElementById('ultima-atualizacao');
-    // Define o texto do elemento para a data e hora atuais formatadas, chamando a função anterior
-    ultimaAtualizacaoElemento.textContent = getDataAtualFormatada();
+// Função para exibir ou ocultar o menu de opções de download
+function toggleDropdown() {
+    var dropdown = document.getElementById("cvDropdown");
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
 }
--- js/contador.js
+++ js/contador.js
@@ -1,3 +1,13 @@
+  // Função para exibir ou ocultar o menu de opções de download
+  function toggleDropdown() {
+    var dropdown = document.getElementById("cvDropdown");
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+
 const contadorBugsElemento = document.getElementById('contador-bugs');
 const contadorEstudoElemento = document.getElementById('contador-estudo');
 const contadorRepositoriosElemento = document.getElementById('contador-repositorios');
@@ -21,7 +31,7 @@ const aumentarContadores = () => {
     contadorEstudo += 5; // Incremento de 5 horas de estudo por dia
     contadorRepositorios += 2; // Incremento de 2 repositórios por dia
     contadorCafe += 4; // Incremento de 4 xícaras de café por dia
-    
+
     // Atualiza os elementos HTML com os novos valores
     contadorBugsElemento.textContent = contadorBugs;
     contadorEstudoElemento.textContent = contadorEstudo;
@@ -45,3 +55,100 @@ const aumentarContadores = () => {
 window.addEventListener('load', () => {
     aumentarContadores();
 });
+  // Função para aumentar os contadores com animação
+  const aumentarContadoresComAnimacao = () => {
+    // Aumenta os contadores...
+    contadorBugs++;
+    contadorEstudo += 5; // Incremento de 5 horas de estudo por dia
+    contadorRepositorios += 2; // Incremento de 2 repositórios por dia
+    contadorCafe += 4; // Incremento de 4 xícaras de café por dia
+
+    // Atualiza os elementos HTML com os novos valores de forma animada
+    animateValue('contador-bugs', 0, contadorBugs, 1000);
+    animateValue('contador-estudo', 0, contadorEstudo, 1000);
+    animateValue('contador-repositorios', 0, contadorRepositorios, 1000);
+    animateValue('contador-cafe', 0, contadorCafe, 1000);
+};
+
+// Função para animar o contador de forma suave
+const animateValue = (id, start, end, duration) => {
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    let obj = document.getElementById(id);
+    let timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+};
+
+// Executa a função de aumentarContadoresComAnimacao imediatamente após a página ser carregada
+window.addEventListener('load', () => {
+    aumentarContadoresComAnimacao();
+});
+
+
+  // Função para fazer a requisição para a API do GitHub e preencher as habilidades
+  async function fetchGitHubData() {
+    const username = 'thmedu'; // Seu nome de usuário no GitHub
+    const apiUrl = `https://api.github.com/users/${username}/repos`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      // Objeto para armazenar as informações das linguagens utilizadas nos repositórios
+      const languages = {};
+
+      // Loop através dos repositórios para contar as linguagens utilizadas
+      data.forEach(repo => {
+        const language = repo.language;
+        if (language && language !== null) {
+          if (languages[language]) {
+            languages[language]++;
+          } else {
+            languages[language] = 1;
+          }
+        }
+      });
+
+      // Preencher dinamicamente as habilidades nas seções correspondentes
+      fillSkills(languages);
+    } catch (error) {
+      console.error('Erro ao recuperar os dados do GitHub:', error);
+    }
+  }
+
+  // Função para preencher dinamicamente as habilidades nas seções correspondentes
+  function fillSkills(languages) {
+    const librariesFrameworksSection = document.getElementById('skills_1');
+    const languagesSection = document.getElementById('skills_2');
+
+    // Limpar qualquer conteúdo existente nas seções
+    librariesFrameworksSection.innerHTML = '';
+    languagesSection.innerHTML = '';
+
+    // Preencher a seção de bibliotecas e frameworks
+    for (const language in languages) {
+      if (Object.hasOwnProperty.call(languages, language)) {
+        const skillValue = languages[language];
+        const skillItem = document.createElement('div');
+        skillItem.classList.add('clearfix');
+        skillItem.innerHTML = `
+          <h4>${language}</h4>
+          <div class="skill-value">${skillValue}%</div>
+          <div class="skill-container" data-value="${skillValue}">
+            <div class="skill-percentage"></div>
+          </div>
+        `;
+        librariesFrameworksSection.appendChild(skillItem);
+      }
+    }
+  }
+
+  // Chamar a função para recuperar os dados do GitHub quando a página carregar
+  window.addEventListener('load', fetchGitHubData);
